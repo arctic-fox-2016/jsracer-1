487129a3124880d4ff40274ee64d60317f1c0530
Fadhli Ramadhani Arislan & Liliwibi <pair+fadhlirams.lilianti@hacktiv8.com>
2016-09-01 14:11:47 +0700

    simpan file

--- dice.js
+++ dice.js
@@ -1,11 +1,24 @@
 "use strict"
 
 class Dice {
-  constructor() {
-
+  constructor(sides) { 
+    this._sides = sides
   }
+
+  get sides(){
+    return this._sides
+  }
+
+  set sides(value){
+    this._sides = value
+  }
+
   roll() {
-    
+    let result = Math.floor(Math.random() * 10);
+    if (result > 6) {
+      result = 6;
+    }
+    return result;
   }
 }
 
--- js_racer.js
+++ js_racer.js
@@ -3,9 +3,42 @@
 import Dice from "./dice.js"
 
 class JSRacer {
-  constructor(players, length, sides) {
+  constructor(players = 3, length = 30, sides = 6) {
+    this._players_number = players;
+    this._length = length;
+    this._sides = sides;
+    this._players_name = [];
+    this._board = [];
+  }
+
+  create_player() {
+    for (let i = 0; i< this.players_number ; i++ ) {
+      players_name.push(i.toString())
+    }
+  }
+
+  create_board() = {
+    for (let i=0; i<this.players_number ;i++) {
+      for (let j=0; j<this)
+    }
+  }
+
+  get length() {
+    return this._length
+  }
+
+  get players_number() {
+    return this._players_number
 
   }
+  get sides(){
+    if (this._sides > 6 ) {
+      return 6
+    } else {
+      return this._sides
+    }
+  }
+
   print_board() {
 
   }
@@ -25,5 +58,8 @@ class JSRacer {
     console.log("\x1B[2J")
   }
 }
+
+let players = new JSRacer([a,b,c], 30, 6);
+
 
 export default JSRacer
