7907f5c38167977db9b7a2e105161cb4d513e32c
Ahyana Rizky Pratama & Septhianto Diga <pair+ahyanarizky.digachandra@hacktiv8.com>
2016-09-01 15:30:26 +0700

    JS Racer

--- js_racer.js
+++ js_racer.js
@@ -2,28 +2,84 @@
 
 import Dice from "./dice.js"
 
+var dadu = new Dice();
+
 class JSRacer {
-  constructor(players, length, sides) {
+  constructor(players, length, sides){
+    this._players = players
+    this._length = length
+    this._sides = sides
+    this._win = false;
+    this._finished = false;
 
+    this._start = false;
+    this._start_index = 0;
+    this._start_count = 3;
   }
-  print_board() {
 
-  }
-  print_line(player, pos) {
+  set players(value){ this._players = value; }
+  get players(){ return this._players; }
+  set length(value){ this._length = value; }
+  get length(){ return this._length; }
+  set sides(value){ this._sides = value; }
+  get sides(){ return this._sides; }
 
-  }
-  advanced_player(player) {
-
-  }
-  finished() {
-
-  }
-  winner() {
-
-  }
   reset_board() {
-    console.log("\x1B[2J")
+    //console.log("\x1B[2J")
+    for(let idx = 0; idx < 50; idx++){ console.log("\n"); }
+    let title = "$$$ ahyanarizky-digachandra NASCAR $$$";
+    let tagline = ["", "Go go go!!", "Vroom Vroom.."];
+
+    if(!this._start){
+      tagline = ["Ready", "Set", "Go"];
+      if(this._start_index < this._start_count){
+        title += " " + tagline[this._start_index];
+        this._start_index += 1;
+      } else {
+        this._start = true;
+      }
+    } else {
+      title += "       " + tagline[Math.floor(Math.random()*tagline.length)];
+    }
+    console.log(title);
+  }
+
+  print_board() {
+    if(!this._win){
+      this.reset_board();
+      for(let idx = 0; idx < this.players.length; idx++){
+        if(this.sides[idx] >= this.length){
+          this._win = true;
+          this._finished = true;
+        }
+
+        this.print_line(this.players[idx], this.sides[idx])
+        if(this._start) this.sides[idx] += dadu.roll();
+      }
+    }
+    return this._win;
+  }
+
+  print_line(player, pos) {
+    let line_length = "";
+    for(let idy = 0; idy < this.length; idy++){
+      let pos_text = " ";
+      if(idy == pos){
+        pos_text = player;
+      }
+      line_length += " | " + pos_text;
+    }
+    if(this._win && this._finished){
+      line_length += player + " Win!!";
+      this._finished = false;
+    }
+    console.log(line_length);
+  }
+
+  run(){
+    return this.print_board();
   }
 }
+
 
 export default JSRacer
--- runner.js
+++ runner.js
@@ -12,3 +12,40 @@ function sleep(milliseconds) {
 }
 
 // Your code here...
+
+var racer = new JSRacer(["A", "B", "C"], 25, [0, 0, 0]);
+function runwei(){
+  setTimeout(function(){
+    if(!racer.run()) runwei();
+  }, 1000);
+}
+runwei();
+/*
+var term = require('terminal-kit').terminal;
+var progressBar, progress = 0;
+
+function doProgress(){
+    // Add random progress
+    progress += Math.random() / 10;
+    progressBar.update(progress);
+    if(progress >= 1){
+        // Cleanup and exit
+        setTimeout(function(){ term('\n');
+          process.exit();
+        }, 200);
+    } else {
+        console.log("asd");
+        setTimeout(doProgress , 100 + Math.random() * 400 );
+    }
+}
+
+
+progressBar = term.progressBar({
+    width:80 ,
+    title:'Serious stuff in progress:',
+    eta:true ,
+    percent:true
+});
+
+doProgress();
+*/
