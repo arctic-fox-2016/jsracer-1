0b4b9242ee463b60ee04fff2415d423d4259b397
Aji Lantang Mardika & Toni <pair+ajilantang.tonychen09@hacktiv8.com>
2016-09-01 20:11:49 +0700

    aji-toni

--- js_racer.js
+++ js_racer.js
@@ -3,38 +3,27 @@
 import Dice from "./dice.js"
 
 class JSRacer {
-  constructor(players, length, sides) {
-this.players = players;
-this.length = length;
-this.sides = sides;
-  }
-  print_board() {
+  constructor(players, panjang , sides) {
+    this.players = players;
+    this.panjang = panjang;
+    this.sides = sides;
+      }
 
-    let kotak = [];
-    for (var i = 0; i < 20; i++) {
-      if (i%2==0) {
-        kotak[i]="|"//indeks genap
+  print_board() {
+    for (var i = 0; i < this.players.length; i++) {
+      let papan = this.players[i]
+      for (var i = 0; i < this.panjang; i++) {
+        papan += "|";
       }
     }
-
-    // for(var i=0;i<kotak.length;i++)
-    // {
-    //     kotak[i] = " ";
-    //     console.log("<br>");
-    //     console.log(kotak[i]);
-    // }
-    //
-    // for(var i=0;var <= kotak.length; var++)
-    // {
-    //   console.log();
-    // }
+    console.log(papan);
   }
   print_line(player, pos) {
-    for (var i = 0; i < 20; i++) {
-      if (i%2!==0) {
-        kotak[dice.step*i]="a"//indeks genap
-      }
-    }
+    // for (var i = 0; i < 20; i++) {
+    //   if (i%2!==0) {
+    //     kotak[dice.step*i]="a"//indeks genap
+    //   }
+    // }
   }
   advanced_player(player) {
       this.player = player;
